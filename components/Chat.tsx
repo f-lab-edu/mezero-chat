7a623afba55cfdeae009362e70938b6d412737ee
nayoung <nayoungace@naver.com>
2024-09-30 11:11:14 +0900

    CLN: role 구분 시 enum 적용

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,10 +1,10 @@
-import { IOpenAiParam } from '@/types/OpenAiParam';
+import { IOpenAiParam, OpenAiRole } from '@/types/OpenAiParam';
 
 export default function Chat({ role, content }: IOpenAiParam) {
   return (
     <div
       className={`flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm ${
-        role === 'user' ? 'ml-auto bg-primary text-primary-foreground' : 'mr-auto bg-muted'
+        role === OpenAiRole.user ? 'ml-auto bg-primary text-primary-foreground' : 'mr-auto bg-muted'
       }`}
     >
       {content}
