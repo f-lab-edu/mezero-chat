8e5c40736e5036df8f8d473d8a748fe0e9cb13ee
nayoung <nayoungace@naver.com>
2024-10-15 10:18:46 +0900

    ENH: Header logo 클릭 시 루트 페이지로 이동 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0">
-          <h1 className="font-bold">Chat</h1>
+          <h1 className="font-bold">
+            <Link href="/">Chat</Link>
+          </h1>
         </div>
       </div>
     </header>
